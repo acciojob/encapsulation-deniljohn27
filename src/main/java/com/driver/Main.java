29fc7bf2fb4e11bc8cfc91ffc3d7925ab3d82762
denil <deniljohnms27@gmail.com>
2023-01-16 08:47:17 +0530

    one

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly r=new RWOnly();
+        //r.name="Denil";
+        //System.out.print(r.name);
+         r.setName("Denil");
+         r.getName();
+    }
 }
